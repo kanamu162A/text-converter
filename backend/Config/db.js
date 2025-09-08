df4bc8fe6a7e30c340c2878d4cb3349c7e386173
kanamu162A <muabubakar715@gmail.com>
2025-09-08 19:20:23 +0100

    deploy backend

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,18 +1,16 @@
 import dotenv from "dotenv";
 import pkg from "pg";
-const { Pool } = pkg;
 
 dotenv.config();
+const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false, 
-  },
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,   // ✅ now it's a real string
+  ssl: { rejectUnauthorized: false },
 });
 
 pool.connect((err, client, release) => {
